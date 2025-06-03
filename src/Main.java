4c81fa693e050887c6b4123910bc6fa753c412e3
Cherry-meat <olaboltikova4@gmail.com>
2025-06-03 23:00:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,22 +1,41 @@
-import java.util.regex.*;
 import java.io.*;
 public class Main {
-    public static void main(String[] args){
-        String regx = "(/\\*[\\s\\S]*?\\*/)|((?<![(:\"\\w])(//.*))";
-        Pattern pattern = Pattern.compile(regx);
-        try (BufferedReader br = new BufferedReader(new FileReader("code.txt"));
-             BufferedWriter bw = new BufferedWriter(new FileWriter("regex.txt"))){
-            StringBuilder file = new StringBuilder();
-            String s;
-            while((s = br.readLine()) != null){
-                file.append(s).append("\n");
+    public static void main(String[] args) {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("reg.txt"));
+            BufferedWriter bw = new BufferedWriter(new FileWriter("regno.txt"));
+            String text;
+            int index;
+            while((text = br.readLine()) != (null)){
+                index = text.indexOf("//");
+                if((index == -1)){
+                    index = text.indexOf("/*");
+                    if (index  == -1){
+                        bw.write(text + "\n");
+                        bw.flush();
+                    }
+                    else{
+                        if (index != 0){
+                            bw.write(text.substring(0, index) + "\n");
+                            bw.flush();
+                        }
+                        else{
+                            while((index = text.indexOf("*/")) == -1){
+                                text = br.readLine();
+                            }
+                            bw.write(text.substring(index + 2) + "\n");
+                            bw.flush();
+                        }
+                    }
+                }
+                else if (index != 0){
+                    bw.write(text.substring(0, index) + "\n");
+                    bw.flush();
+                }
             }
-            Matcher matcher = pattern.matcher(file.toString());
-            String result = matcher.replaceAll("");
-            bw.write(result);
         }
-        catch (IOException e){
-            e.printStackTrace();
+        catch (Exception ex){
+            ex.printStackTrace();
         }
     }
 }
